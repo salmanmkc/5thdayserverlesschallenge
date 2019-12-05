58c506a31070439c7b2b41a2dfa9caba1e8d4895
SalmanMKC <13schishti@gmail.com>
2019-12-05 16:05:26 +0000

    deserialise the message based on json provided

---
+++ ConsoleApp4/messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class messages
+    {
+
+        public class Rootobject
+        {
+            public Class1[] Property1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public string who { get; set; }
+            public string message { get; set; }
+            public string mssage { get; set; }
+        }
+
+    }
+}
