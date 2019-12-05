dea380d7d1d40f4032f8fec17620df0197c1d405
SalmanMKC <13schishti@gmail.com>
2019-12-05 16:41:11 +0000

    changed name to httpclient instead

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -30,10 +30,10 @@ namespace ConsoleApp4
             string GetRequestURL = "https://christmaswishes.azurewebsites.net/api/Wishes";
 
             //create an instance of HttpClient
-            HttpClient jsonclient = new HttpClient();
+            HttpClient httpclient = new HttpClient();
 
             //get the reponse as JSON format
-            string response = await jsonclient.GetStringAsync(GetRequestURL);
+            string response = await httpclient.GetStringAsync(GetRequestURL);
             string newResponse = @"{ ""children"":" + response + "}";
 
             //parse the data
